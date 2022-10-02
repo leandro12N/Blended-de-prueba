ea3d08f82fd1895f00a57551d9326f07a615f079
Lean <lean@Enricos-MacBook-Pro.local>
2022-10-02 04:44:56 -0300

    crud: edit,destroy y multer

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -9,14 +9,13 @@ function findAll() {
   return data;
 }
 
-function create (data){
+function writeFile (data){
     const dataString = JSON.stringify(data, null, 4);
     fs.writeFileSync(path.join(__dirname, "../data/productos.json"), dataString);
 }
 const controllers = {
   list: function (req, res) {
     const data = findAll();
-    console.log(data);
     res.render("menu-productos", { productos: data });
   },
   detail: function (req, res) {
@@ -32,19 +31,54 @@ const controllers = {
   },
   store: function (req, res) {
     const data = findAll();
+    console.log(req.file);
     const newProduct = {
       id: data.length + 1,  //Del largo de mi array osea la cantidad de element le sumo 1 para que no pide los demas elementos
       name: req.body.name,
       price: Number(req.body.price), //Number transforma el string que me llega en numero
       description: req.body.description,
+      image: req.file.filename
     };
 
     data.push(newProduct);
 
-    create(data);
+    writeFile(data);
 
     res.redirect("/products/list");
   },
+  edit: function(req,res){
+    const data = findAll();
+    const cafeEncontrado = data.find(function(cafe){
+      return cafe.id == req.params.id
+    })
+    res.render("editar-productos", { cafe: cafeEncontrado})
+  },
+  update: function(req,res){
+    const data = findAll();
+    const cafeEncontrado = data.find(function(cafe){
+      return cafe.id == req.params.id
+    })
+
+    cafeEncontrado.name = req.body.name;
+    cafeEncontrado.price = req.body.price;
+    cafeEncontrado.description = req.body.description;
+
+    writeFile(data);
+
+    res.redirect("/products/list");
+  },
+  destroy: function(req,res){
+    const data = findAll();
+    const datoEncontrado = data.findIndex(function(cafe){
+      return cafe.id == req.params.id
+    })
+
+    data.splice(datoEncontrado, 1);
+
+    writeFile(data);
+
+    res.redirect("/products/list");
+  }
 };
 
 module.exports = controllers;
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const productController = require ("../controllers/productControllers");
+const productController = require("../controllers/productControllers");
+const multer = require("multer");
+const path = require("path")
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, "../public/img/imagenes"))
+    },
+    filename: function (req, file, cb) {
+        const newFileName = file.fieldname + Date.now() + '-' + path.extname(file.originalname)
+        cb(null, newFileName)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+
 
 router.get("/list", productController.list);
 router.get("/detail/:id", productController.detail);
 router.get("/create", productController.create);
-router.post("/create", productController.store)
+router.post("/create", upload.single("imagen") ,productController.store);
+router.get("/edit/:id", productController.edit);
+router.put("/edit/:id", productController.update);
+router.delete("/delete/:id", productController.destroy)
 
 module.exports = router;
